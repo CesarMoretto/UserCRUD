04dbde6a3879c6b79d2c1b29301c28009bc32711
Cesar Moretto <81265167+CesarMoretto@users.noreply.github.com>
2024-07-11 12:32:10 -0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+--DROP DATABASE CadastroUsuarioDB
+
+CREATE DATABASE CadastroUsuarioDB;
+
+USE CadastroUsuarioDB;
+
+CREATE TABLE usuarios (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    nome NVARCHAR(100) NOT NULL,
+    email NVARCHAR(100) NOT NULL,
+    senha NVARCHAR(100) NOT NULL
+);
+
+SELECT * FROM usuarios;
+
